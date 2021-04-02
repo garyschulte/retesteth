01a874e2c60b70f4aaa0e7707d428dc431c8b37b
garyschulte <garyschulte@gmail.com>
2021-04-02 09:43:22 -0700

    more signature tweaking

--- retesteth/testStructures/types/Ethereum/TransactionAccessList.cpp
+++ retesteth/testStructures/types/Ethereum/TransactionAccessList.cpp
@@ -124,7 +124,7 @@ DataObject const TransactionAccessList::asDataObject(ExportOrder _order) const
     return out;
 }
 
-string TransactionAccessList::getTxDataString() const override
+string TransactionAccessList::getTxDataString() const 
 {
     return m_txDataString;
 }
